b4bef0c48bd255759874817975562c91abcb46a6
Alyaluthfi <161783671+Alyaluthfi@users.noreply.github.com>
2024-03-05 21:10:51 +0700

    Add files via upload

---
+++ Unit 2/latihan unit2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+//c
+int main() {
+    
+    int jumlahTransaksi, nominalTransaksi, totalTransaksi = 0;
+
+   
+    printf("Masukkan jumlah transaksi: ");
+    scanf("%d", &jumlahTransaksi);
+
+   
+    if (jumlahTransaksi <= 0) {
+        printf("Tidak ada transaksi hari ini\n");
+        return 0;
+    }
+
+
+    for (int i = 0; i < jumlahTransaksi; i++) {
+        printf("Masukkan nominal transaksi ke-%d: ", i + 1);
+        scanf("%d", &nominalTransaksi);
+        totalTransaksi += nominalTransaksi;
+    }
+
+  
+    printf("Total pengeluaran hari ini: %d\n", totalTransaksi);
+
+    return 0;
+}
